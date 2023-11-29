300fd8dbf0ff08e124ac43548a4706a37780382b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-11-29 18:41:59 +0900

    231129_이미지 가져오기, 백그라운드, spaceship 이미지 렌더

--- main.js
+++ main.js
@@ -8,3 +8,40 @@ canvas.width = 400;
 canvas.height = 700;
 
 document.body.appendChild(canvas);
+
+//이미지 가져와 보여주기
+let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
+
+//우주선 좌표
+let spaceshipX = canvas.width / 2 - 33;
+let spaceshipY = canvas.height - 114;
+
+function loadImage() {
+  backgroundImage = new Image();
+  backgroundImage.src = "images/background.jpg";
+
+  spaceshipImage = new Image();
+  spaceshipImage.src = "images/spaceship.png";
+
+  bulletImage = new Image();
+  bulletImage.src = "images/bullet.png";
+
+  enemyImage = new Image();
+  enemyImage.src = "images/enemy.png";
+
+  gameOverImage = new Image();
+  gameOverImage.src = "images/gameover.png";
+}
+
+function render() {
+  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY, 66, 66);
+}
+
+function main() {
+  render();
+  requestAnimationFrame(main);
+}
+
+loadImage();
+main();
